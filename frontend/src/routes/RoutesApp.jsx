17264afdb112cec456b57b291b93fefa2546291d
Manuel.Millan1979 <nutriglar@gmail.com>
2025-02-28 19:43:02 -0300

    cambios en la configuracion

---
+++ frontend/src/routes/RoutesApp.jsx
@@ -0,0 +1,18 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import LoginPage from '../pages/LoginPage'
+import SignupPage from '../pages/SignupPage.jsx'
+import ForgotPasswordPage from '../pages/ForgotPasswordPage.jsx'
+
+
+const RoutesApp = () => {
+return (
+    <Routes>
+    <Route path='/' element={<Navigate to='/login' />}></Route>
+    <Route path='/login' element={<LoginPage />}></Route>
+    <Route path='/signup' element={<SignupPage />} />
+    <Route path='/forgot-password' element={<ForgotPasswordPage />} />
+    </Routes>
+)
+}
+
+export default RoutesApp
