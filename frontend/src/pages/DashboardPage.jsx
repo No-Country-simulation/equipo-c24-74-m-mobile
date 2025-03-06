00de6489f445e3f888955a66d8e3f52d0e2a85aa
Roman Ramos <romanrv1217@gmail.com>
2025-03-06 08:04:55 -0600

    Se arreglo el navbar, ahora aparece correctamente en dashboard

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,11 +1,20 @@
 import SearchBar from '../components/Dashboard/SearchBar/SearchBar';
 import DashboardGrid from '../components/Dashboard/DashboardGrid/DashboardGrid';
+import Navbar from '../components/NavbarLeft/NavbarLeft';
 
 const DashboardPage = () => {
     return (
-        <div className="bg-blue-100 min-h-screen p-4">
-            <SearchBar />
-            <DashboardGrid />
+        <div className="bg-blue-100 min-h-screen flex">
+           
+            <div className="w-1/6 bg-white">
+                <Navbar />
+            </div>
+
+          
+            <div className="flex-1 p-4">
+                <SearchBar />
+                <DashboardGrid />
+            </div>
         </div>
     );
 };
